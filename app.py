9f4778327c78bc34055b17b13ede3bc97631f905
Cherniak Tamir <tamircher123@gmail.com>
2018-12-02 00:14:30 +0200

    little fix

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from models import db, Rooms, Player
 from flask import Flask, jsonify, request
 from flask_socketio import SocketIO, join_room
 
@@ -9,49 +10,53 @@ app.config.from_object(__name__)
 app.config['MONGODB_SETTINGS'] = {'DB': 'Connect4'}
 
 app.config['DEBUG'] = True
-from models import db, Rooms, Player
 db.init_app(app)
+
 
 @app.route("/")
 def hello():
     return 'reuven'
 
+
 @app.route('/rooms')
 def get_rooms():
-   rooms = Rooms.objects.all()
-   return jsonify(rooms), 200
+    rooms = Rooms.objects.all()
+    return jsonify(rooms), 200
+
 
 @socketio.on('createRoom')
 def create_room(user_args):
 
-   board = [[''] * 4] * 4
-   player1 = Player(name=user_args['name'], color=user_args['color'], sid=request.sid)
+    board = [[''] * 4] * 4
+    player1 = Player(name=user_args['name'],
+                     color=user_args['color'], sid=request.sid)
 
-   room = Rooms(board=board, players=[player1])
-   room.save()
+    room = Rooms(board=board, players=[player1])
+    room.save()
 
-   join_room(str(room.id))
+    join_room(str(room.id))
 
-   socketio.emit("roomOpened", {'roomId': str(room.id)})
-   
-   return {id: room.id}
+    socketio.emit("roomOpened", {'roomId': str(room.id)})
+
+    return 'ok'
+
 
 @socketio.on('joinRoom')
-def join_room(room_id, user_args):
-   
-   room = Rooms.objects(id=room_id)
-   if room:
-      
-      if(len(room.players) == 2)
-         return 'Game is progress'
+def join_game_room(room_id, user_args):
 
-      player = Player(name=user_args['name'], color=user_args['color'], sid=request.sid)
-      join_room(room_id)
-      socketio.emit("startGame", {'roomData': jsonify(room)}, room=room_id)
-   else:
-      return 'No such room'
+    room = Rooms.objects(id=room_id)
+    if room:
 
-   
+        if len(room.players) == 2:
+            return 'Game is progress'
+
+        player = Player(name=user_args['name'],
+                        color=user_args['color'], sid=request.sid)
+        join_room(room_id)
+        socketio.emit("startGame", {'roomData': jsonify(room)}, room=room_id)
+    else:
+        return 'No such room'
+
 
 if __name__ == '__main__':
     app.debug = app.config['DEBUG']
