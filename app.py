d531c851dab78a0c12fe6cf7b3594fc9af5bc27d
Tamir <tamircher123@gmail.com>
2018-12-01 14:49:23 +0200

    Room model and basic get rooms, add rooms functions

--- app.py
+++ app.py
@@ -7,16 +7,26 @@ app.config['MONGODB_SETTINGS'] = {'DB': 'Connect4'}
 app.config['SECRET_KEY'] = 'reuvenKing!'
 app.config['DEBUG'] = True
 
-from models import db, Board
+from models import db, Rooms, Player
 db.init_app(app)
 
 @app.route("/")
 def hello():
-    return jsonify(Board.objects.all()), 200
+    return 'reuven'
 
 @app.route('/rooms')
 def get_rooms():
-   pass
+   rooms = Rooms.objects.all()
+   return jsonify(rooms), 200
+
+@app.route('/add_room')
+def add_room():
+   board = [[''] * 4] * 4
+   player1 = Player(name='Omer', color='#7B7B7B')
+
+   room = Rooms(board=board, player1=player1)
+   room.save()
+   return 'ok', 200
 
 if __name__ == '__main__':
     app.debug = app.config['DEBUG']
--- models.py
+++ models.py
@@ -5,6 +5,13 @@ import mongoengine as me
 
 db = MongoEngine()
 
-class Board(db.Document):
-    board = me.ListField()
-    
+class Player(me.EmbeddedDocument):
+    name = me.StringField(max_length=50)
+    color = me.StringField(max_length=7, default='#424242')
+
+class Rooms(db.Document):
+    board = me.ListField(me.ListField(me.StringField(max_length=1)))
+    isOpen = me.BooleanField(default=True)
+    player1 = me.EmbeddedDocumentField(Player)
+    player2 = me.EmbeddedDocumentField(Player)
+    turn = me.BooleanField(default=True)
